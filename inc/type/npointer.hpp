01783a001a2230104b53fa84d8bae44c98c19b48
chenqiudu <chenqiudu@kuaishou.com>
2025-04-24 11:30:34 +0800

    delete pointer get target

--- inc/type/npointer.hpp
+++ inc/type/npointer.hpp
@@ -12,7 +12,6 @@ public:
 
     static void* get_value(const class nobject& pointer);
     static void set_value(nobject& pointer, void* value);
-    static nwrapper get_target(const nobject& pointer);
 
     NTR_INLINE uint8_t depth() const { return _depth; }
     NTR_INLINE const ntype* pointed_type() const { return _pointed_type; }
--- src/type/npointer.cpp
+++ src/type/npointer.cpp
@@ -19,17 +19,6 @@ void npointer::set_value(nobject& pointer, void* value)
     *reinterpret_cast<void**>(pointer.data()) = value;
 }
 
-nwrapper npointer::get_target(const nobject& pointer)
-{
-    if (!pointer.type()->is_pointer())
-        throw std::invalid_argument(
-            "npointer::get_target : pointer's type is not pointer type");
-    void* value = get_value(pointer);
-    if (value == nullptr)
-        throw std::invalid_argument("npointer::get_target : pointer's value is nullptr");
-    return nwrapper(pointer.type()->as_pointer()->pointed_type(), value);
-}
-
 npointer::npointer(uint8_t depth, const ntype* pointed_type, std::string_view name,
                    operations* ops)
     : ntype(ntype::etype::epointer, sizeof(void*), alignof(void*), ops, name),
