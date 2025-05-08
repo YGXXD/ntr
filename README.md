5decc6ef45a2b129aba72599f810ddfe5b7a6e64
chenqiudu <chenqiudu@kuaishou.com>
2025-05-08 17:55:49 +0800

    add exemple

--- README.md
+++ README.md
@@ -42,3 +42,63 @@ add_executable(exemple main.cpp)
 find_package(ntr REQUIRED)
 target_link_libraries(exemple PUBLIC ntr::ntr)
 ```
+
+### 示例 
+
+```c++
+#include "<ntr/nephren.hpp>"
+#include <iostream>
+
+using namespace ntr;
+
+class kutori
+{
+public:
+    void update_info(float in_height, float in_weight)
+    {
+        height = in_height;
+        weight = in_weight;
+    }
+
+    const int& age() const { return _age; }
+    void set_age(const int& age) { _age = age; }
+
+    float height;
+    float weight;
+
+private:
+    int _age;
+};
+
+int main()
+{
+    // 注册珂朵莉类型
+    nephren::type<kutori>("kutori")
+        .property("age", &kutori::age, &kutori::set_age)
+        .property("height", &kutori::height)
+        .property("weight", &kutori::weight)
+        .function("update_info", &kutori::update_info);
+
+    // 获取珂朵莉类型
+    const nclass* kutori_type = nephren::get<kutori>();
+    // 可以通过注册名动态获取
+    const nclass* kutori_type_by_name = nephren::get("kutori")->as_class();
+
+    // 调用珂朵莉实例的成员函数
+    nobject kutori_obj = kutori_type->new_instance();
+    kutori_type->call("update_info", { kutori_obj.wrapper(), 160.0f, 45.0f });
+
+    // 设置珂朵莉实例的成员变量
+    kutori_type->set("age", kutori_obj.wrapper(), 15);
+
+    // 获取珂朵莉实例的成员变量
+    nobject height = kutori_type->get("height", kutori_obj.wrapper());
+    nobject weight = kutori_type->get("weight", kutori_obj.wrapper());
+    nobject age = kutori_type->get("age", kutori_obj.wrapper());
+    std::cout << "珂朵莉的身高是: " << height.as<float>() << "cm" << std::endl;
+    std::cout << "珂朵莉的体重是: " << weight.as<float>() << "kg" << std::endl;
+    std::cout << "珂朵莉的年龄是: " << age.as<int>() << "岁" << std::endl;
+
+    return 0;
+}
+```
--- test/ntr_test.cpp
+++ test/ntr_test.cpp
@@ -1,168 +1,55 @@
 #include "nephren.hpp"
 #include <iostream>
 
-static int nephren_register_numeric = []() -> int
-{
-    ntr::nephren::type<int8_t>("int8");
-    ntr::nephren::type<int16_t>("int16");
-    ntr::nephren::type<int32_t>("int32");
-    ntr::nephren::type<int64_t>("int64");
-    ntr::nephren::type<uint8_t>("uint8");
-    ntr::nephren::type<uint16_t>("uint16");
-    ntr::nephren::type<uint32_t>("uint32");
-    ntr::nephren::type<uint64_t>("uint64");
-    ntr::nephren::type<float>("float");
-    ntr::nephren::type<double>("double");
+using namespace ntr;
 
-    ntr::nephren::type<int8_t*>("int8*");
-    ntr::nephren::type<int16_t*>("int16*");
-    ntr::nephren::type<int32_t*>("int32*");
-    ntr::nephren::type<int64_t*>("int64*");
-    ntr::nephren::type<uint8_t*>("uint8*");
-    ntr::nephren::type<uint16_t*>("uint16*");
-    ntr::nephren::type<uint32_t*>("uint32*");
-    ntr::nephren::type<uint64_t*>("uint64*");
-    ntr::nephren::type<float*>("float*");
-    ntr::nephren::type<double*>("double*");
-    return 0;
-}();
-
-enum class MyEnum
-{
-    MyEnum1 = 1,
-    MyEnum2 = 2,
-    MyEnum3 = 3
-};
-
-class MyFather
+class kutori
 {
 public:
-    int father_value = 6;
-};
-
-class MyChild : public MyFather
-{
-public:
-    int child_value = 2;
-};
-
-class MyClass
-{
-public:
-    const int& value() const { return _value; }
-    void value(const int& value) { _value = value; }
-
-    static void print(MyFather& x)
+    void update_info(float in_height, float in_weight)
     {
-        std::cout << "MyClass" << x.father_value << std::endl;
+        height = in_height;
+        weight = in_weight;
     }
 
-    double& print_member(double& x)
-    {
-        x -= 2.5;
-        std::cout << "MyClass" << x << std::endl;
-        return x;
-    }
+    const int& age() const { return _age; }
+    void set_age(const int& age) { _age = age; }
 
-    double float_value;
+    float height;
+    float weight;
 
 private:
-    int _value;
+    int _age;
 };
 
-static void xxxxx(MyChild& x)
+int main()
 {
-    std::cout << "MyClass" << x.father_value << std::endl;
-}
+    // 注册珂朵莉类型
+    nephren::type<kutori>("kutori")
+        .property("age", &kutori::age, &kutori::set_age)
+        .property("height", &kutori::height)
+        .property("weight", &kutori::weight)
+        .function("update_info", &kutori::update_info);
 
-int main(int argc, char* argv[])
-{
-    ntr::nephren::type<MyEnum>("MyEnum")
-        .item("MyEnum1", MyEnum::MyEnum1)
-        .item("MyEnum2", MyEnum::MyEnum2)
-        .item("MyEnum3", MyEnum::MyEnum3);
+    // 获取珂朵莉类型
+    const nclass* kutori_type = nephren::get<kutori>();
+    // 可以通过注册名动态获取
+    const nclass* kutori_type_by_name = nephren::get("kutori")->as_class();
 
-    ntr::nephren::type<MyClass>("MyClass")
-        .function("print", &MyClass::print)
-        .property("float_value", &MyClass::float_value)
-        .property("value", &MyClass::value, &MyClass::value)
-        .function("print_member", &MyClass::print_member);
+    // 调用珂朵莉实例的成员函数
+    nobject kutori_obj = kutori_type->new_instance();
+    kutori_type->call("update_info", { kutori_obj.wrapper(), 160.0f, 45.0f });
 
-    std::cout << ntr::nephren::get<uint64_t>()->name() << std::endl;
-    std::cout << static_cast<int>(ntr::nephren::get("int8")->as_numeric()->numeric_kind())
-              << std::endl;
+    // 设置珂朵莉实例的成员变量
+    kutori_type->set("age", kutori_obj.wrapper(), 15);
 
-    const ntr::nenum* enum_type = ntr::nephren::get("MyEnum")->as_enum();
-    std::cout << enum_type->get_eitem(static_cast<long>(MyEnum::MyEnum3))->value()
-              << std::endl;
-
-    const ntr::nclass* class_type = ntr::nephren::get<MyClass>()->as_class();
-    std::cout << class_type->get_property("float_value")->property_type()->name()
-              << std::endl;
-    std::cout << class_type->get_function("print")->name() << std::endl;
-
-    MyClass my_class;
-    my_class.value(10);
-    my_class.float_value = 0;
-
-    const ntr::nproperty* property = class_type->get_property("value");
-    std::cout << property->property_type()->name() << std::endl;
-    std::cout << property->get(my_class).as<int>() << std::endl;
-    property->set(my_class, 20);
-    std::cout << property->get(my_class).as<int>() << std::endl;
-
-    property = class_type->get_property("float_value");
-    std::cout << property->property_type()->name() << std::endl;
-    std::cout << property->get(my_class).as<double>() << std::endl;
-    property->set(my_class, double(20));
-    std::cout << property->get(my_class).as<double>() << std::endl;
-
-    const ntr::nfunction* function = class_type->get_function("print");
-    std::cout << function->name() << std::endl;
-    std::cout << function->return_type()->name() << std::endl;
-    std::cout << function->argument_types().size() << std::endl;
-
-    std::cout << function->invoke({ static_cast<MyFather&&>(MyChild()) }).type()->name()
-              << std::endl;
-
-    function = class_type->get_function("print_member");
-    std::cout << function->name() << std::endl;
-    std::cout << function->return_type()->name() << std::endl;
-    std::cout << function->argument_types().size() << std::endl;
-    double x = 19.9;
-    std::vector<ntr::nwrapper> args { my_class, x };
-    function->invoke(args);
-    auto result = function->invoke(args);
-    result.as<double>() = 10;
-    std::cout << result.as<double>() << std::endl;
-    std::cout << x << std::endl;
-
-    ntr::nwrapper ref = ntr::nwrapper(100);
-    std::cout << ref.cref<int>() << std::endl;
-    std::unique_ptr<int> a = std::make_unique<int>(100);
-    a = std::make_unique<int>(200);
-
-    ntr::nobject obj = ntr::nobject::make_obtain<int>(100);
-
-    ntr::nnumeric::set_value(obj, 205.2);
-    std::cout << ntr::nnumeric::get_value(obj) << std::endl;
-
-    // long lll = 100;
-    // MyEnum eee = static_cast<MyEnum>(lll);
-    // std::cout << sizeof(ntr::nobject) << std::endl;
-    // ntr::nobject obj = ntr::nobject(MyChild());
-    // ntr::nobject obj2 = obj.clone();
-    // ntr::nobject obj3 = obj.steal();
-
-    std::cout << ntr::nephren::get<int8_t*>()->name() << std::endl;
-    std::cout << ntr::nephren::get<int16_t*>()->name() << std::endl;
-
-    uint16_t u16 = 100;
-    ntr::nobject obj2 = ntr::nephren::get<uint16_t*>()->new_object();
-    ntr::npointer::set_value(obj2, &u16);
-    std::cout << ntr::npointer::get_target(obj2).as<uint16_t>() << std::endl;
-
-    std::cout << std::is_copy_constructible_v<int[10]> << std::endl;
+    // 获取珂朵莉实例的成员变量
+    nobject height = kutori_type->get("height", kutori_obj.wrapper());
+    nobject weight = kutori_type->get("weight", kutori_obj.wrapper());
+    nobject age = kutori_type->get("age", kutori_obj.wrapper());
+    std::cout << "珂朵莉的身高是: " << height.as<float>() << "cm" << std::endl;
+    std::cout << "珂朵莉的体重是: " << weight.as<float>() << "kg" << std::endl;
+    std::cout << "珂朵莉的年龄是: " << age.as<int>() << "岁" << std::endl;
 
     return 0;
 }
